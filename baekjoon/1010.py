87989c458673bb60a3dac35b8920150f90d64f42
조수훈 <hun@josuhun-ui-MacBookAir.local>
2023-07-20 22:02:59 +0900

    secont_commit

--- baekjoon/1010.py
+++ baekjoon/1010.py
@@ -1,10 +1,25 @@
 T = int(input())
 
+
+def fact(i):
+    if i == 0:
+        return 1
+    return i * fact(i-1)
+
+
 for tc in range(T):
     N, M = map(int, input().split())
 
-    def fact(M):
-        if M == 0:
-            return 1
-        return M * fact(M-1)
-    print(fact(M))
+    fact_N = fact(N)
+    fact_M = fact(M)
+    fact_MN = fact(M-N)
+
+    print(int(fact_M / (fact_N * fact_MN)))
+
+    # M!
+    # N!
+    # (M-N)!
+    # def factorial(n):
+    # if n == 0:          # 무한 호출을 조심해!
+    # 	return 1
+    # return n * factorial(n-1)
