2de89f0da5b6291cf10fb6bd5ed07e09b631df1b
조수훈 <hun@josuhun-ui-MacBookAir.local>
2023-07-26 22:33:59 +0900

    23_07_26_commit

--- baekjoon/1251.py
+++ baekjoon/1251.py
@@ -1,3 +1,14 @@
 st = input()
-st_lst = list(st)
-print(sorted(st_lst))
+
+lst = []
+for i in range(1, len(st)-2):
+    for j in range(i+1, len(st)):
+        st1 = st[:i]
+        st2 = st[i:j]
+        st3 = st[j:]
+        re_st1 = st1[::-1]
+        re_st2 = st2[::-1]
+        re_st3 = st3[::-1]
+        lst.append(re_st1+re_st2+re_st3)
+
+print(sorted(lst)[0])
