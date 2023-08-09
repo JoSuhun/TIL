9c5fe0097bc0aff690ea4e6efc6e27202e41cc68
josuhun <hun@josuhun-ui-MacBookAir.local>
2023-08-10 00:05:35 +0900

    23_08_10_commit

---
+++ baekjoon/2018.py
@@ -0,0 +1,22 @@
+# n = int(input())
+#
+# cnt = 1
+# start = 1
+# end = 1
+# SUM = 1
+#
+# while end != n:
+#     if SUM == n:
+#         cnt += 1
+#         end += 1
+#         SUM += end
+#     elif SUM > n:
+#         SUM -= start
+#         start += 1
+#     elif SUM < n:
+#         end += 1
+#         SUM += end
+#
+# print(cnt)
+#
+
