73e67ea272844ae50e350b2457701ac8b0795171
JoSuhun <chosh9128@gmail.com>
2023-08-14 12:02:39 +0900

    23_08_14_commit

--- baekjoon/2018.py
+++ baekjoon/2018.py
@@ -1,22 +1,20 @@
-# n = int(input())
-#
-# cnt = 1
-# start = 1
-# end = 1
-# SUM = 1
-#
-# while end != n:
-#     if SUM == n:
-#         cnt += 1
-#         end += 1
-#         SUM += end
-#     elif SUM > n:
-#         SUM -= start
-#         start += 1
-#     elif SUM < n:
-#         end += 1
-#         SUM += end
-#
-# print(cnt)
-#
+n = int(input())
 
+cnt = 1
+start = 1
+end = 1
+SUM = 1
+
+while end != n:
+    if SUM < n:
+        end += 1
+        SUM += end
+    elif SUM == n:
+        cnt += 1
+        SUM -= start
+        start +=1
+    elif SUM > n:
+        start += 1
+        SUM -= start
+
+print(cnt)
