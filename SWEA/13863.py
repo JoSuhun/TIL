425eeb5e596efa9122b929c8dcf572588708cd8e
JoSuhun <chosh9128@gmail.com>
2023-08-17 08:41:33 +0900

    23_08_17_commit

--- SWEA/13863.py
+++ SWEA/13863.py
@@ -4,10 +4,15 @@ for i in range(1, T+1):
     N = int(input())
     arr = [list(input()) for _ in range(N)]
 
-
-
-
-
-
     for i in range(N):
-        if arr[]
+        if arr[N-1][i] == 2:
+            y, x = N-1, i
+
+    directy = [0, 0, 1, -1]
+    directx = [1, -1, 0, 0]
+    for i in range(4):
+        dy = y + directy[i]
+        dx = x + directx[i]
+
+        if dy<0 or dx<0 or dy>=N or dx>=N: continue
+        if arr[dy][dx] != 0: continue
