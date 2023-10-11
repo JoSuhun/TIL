aa1e1f62b007685bd027f502e57033200e03fa7d
Josuhun <chosh9128@gmail.com>
2023-10-11 17:40:12 +0900

    23_10_11_commit

--- baekjoon/15686.py
+++ baekjoon/15686.py
@@ -1,28 +1,42 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())    # n*n 도시 // 치킨집 최대 m개
 mapp = [list(map(int, input().split())) for _ in range(n)]
 
-chicken = []
 home = []
+chicken = []
 for i in range(n):
     for j in range(n):
-        if mapp[i][j] == 2:
-            chicken.append((i, j))
         if mapp[i][j] == 1:
             home.append((i, j))
+        if mapp[i][j] == 2:
+            chicken.append((i, j))
 
-def find(sty, stx, edy, edx):
-    q = deque([sty, stx, ])
-    used = [[0]*n for _ in range(n)]
+choice = deque([])
+def find(start, cnt):
+    global MIN, check
+    if cnt == m:
+        total = 0
+        for h in home:
+            hy, hx = h
+            dist = 21e8
+            for i in range(len(chicken)):
+                if check[i] == 1:
+                    cy, cx = chicken[i]
+                    dist = min(dist, abs(hy-cy)+abs(hx-cx))
+            total += dist
+            if total > MIN: return
+        MIN = min(MIN, total)
+        return
 
-    directy = [0, 0, 1, -1]
-    directx = [1, -1, 0, 0]
+    for idx in range(start, len(chicken)):
+        check[idx] = 1
+        find(start+1, cnt+1)
+        check[idx] = 0
 
-    while q:
-        nowy, nowx = q.popleft()
-        used[nowy][nowx] = 1
-        for i in range(4):
-            dy = nowy + directy[i]
-            dx = nowx + directx[i]
-            if dy<0 or dx<0 or dy>n-1 or dx>n-1: continue
-            if used[dy][dx] == 1: continue
+check = [0]*(len(chicken)+1)
+MIN = 21e8
+for i in range(len(chicken)):
+    find(i, 0)
+print(MIN)
