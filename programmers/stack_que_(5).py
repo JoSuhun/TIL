6b7b70a4dc2e525b73774d76fd385891113365f9
josuhun <chosh9128@gmail.com>
2024-02-28 21:33:07 +0900

    24_2_28_programmers

--- programmers/stack_que_(5).py
+++ programmers/stack_que_(5).py
@@ -1,21 +1,28 @@
-bridge_length = 2
-weight = 10
-truck_weights = [7,4,5,6]
+bridge_length = 100
+weight = 100
+truck_weights = [10,10,10,10,10,10,10,10,10,10]
 
+from collections import deque
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    onbridge = []
-    temp = 0
-
-    onbridge.append(truck_weights[0])
-    truck_weights.pop(0)
-
+    answer = 1
+    onbridge = deque()
+    ready = deque(truck_weights)
+    new = ready.popleft()
+    onbridge.append([new, answer])
+    temp = new
+    done = 0
     while onbridge:
-        if onbridge[0] >= weight:
-            temp += 1
-        if temp == bridge_length:
-            answer += temp
-            temp = 0
-        
+        answer += 1
+        if answer == bridge_length + onbridge[0][1]:
+            temp -= onbridge[0][0]
+            onbridge.popleft()
+            done += 1
+        if ready:
+            if (temp + ready[0] <= weight) and (len(onbridge) < bridge_length):
+                new = ready.popleft()
+                onbridge.append([new, answer])
+                temp += new
 
     return answer
+
+print(solution(bridge_length, weight, truck_weights))
